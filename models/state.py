1dc9c4b0bf540bb8aa9e52f7dafe97ae29ae6f39
favemma <ayoaborisade@yahoo.com>
2023-03-21 02:44:08 -0700

    Fix the state getter and added Crete_at and update_at

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the state class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
+import os
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete')
+
+    else:
+        @property
+        def cities(self):
+            """Return cities"""
+            all_cities = models.storage.all(City)
+            all_cities_state = []
+            for key, value in all_cities.items():
+                if self.id == value.state_id:
+                    all_cities_state.append(value)
+            return all_cities_state
